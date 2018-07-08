11c00da80cb3524e47f63f0f84fbfa2bdd74d1ab
matt <matthieulentin@icloud.com>
2018-07-08 00:36:08 +0000

    action show and new complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,21 @@
 class ArticlesController < ApplicationController
     def new 
-        @articles = Article.new
+        @article = Article.new
     end
     
     def create
-        #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            flash[:notice]="Article bel et bien créé"
+            redirect_to article_path(@article)
+        else
+            #on affiche un message d'erreur
+            render 'new'
+        end
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
     private

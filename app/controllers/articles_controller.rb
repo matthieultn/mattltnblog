2c5a27d89229ca86083256a0ae5255d091a3a93a
matt <matthieulentin@icloud.com>
2018-07-08 12:14:14 +0000

    Ajout des commentaires

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,46 @@
 class ArticlesController < ApplicationController
     def new 
+        # On récupère un nouvel article
         @article = Article.new
     end
     
     def create
+        # On crée un nouvel article
         @article = Article.new(article_params)
+        
+        # Si l'article se modifie on affiche un message à l'utilisateur et on le redirige vers son article
         if @article.save
             flash[:notice]="Article bel et bien créé"
             redirect_to article_path(@article)
+            
         else
-            #on affiche un message d'erreur
+            # On reste sur la page new
             render 'new'
         end
     end
     
     def show
+        # On récupère l'ID de l'article
         @article = Article.find(params[:id])
+    end
+    
+    def edit
+        # On récupère l'ID de l'article
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        # On récupère l'ID de l'article
+        @article = Article.find(params[:id])
+        
+        # Si l'article se modifie on affiche un message à l'utilisateur et on le redirige vers son article
+        if @article.update
+            flash[:notice]="Article bel et bien modifié"
+            redirect_to article_path(@article)
+        else
+            # On reste sur la page edit
+            render 'edit'
+        end
     end
     
     private
